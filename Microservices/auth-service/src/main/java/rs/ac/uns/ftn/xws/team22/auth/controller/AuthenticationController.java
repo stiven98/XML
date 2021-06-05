6db54b5678b8b2a11051746087580294be3332ee
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2021-06-05 17:34:39 +0200

    Gateway configuration added

--- Microservices/auth-service/src/main/java/rs/ac/uns/ftn/xws/team22/auth/controller/AuthenticationController.java
+++ Microservices/auth-service/src/main/java/rs/ac/uns/ftn/xws/team22/auth/controller/AuthenticationController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.xws.team22.auth.dto.AuthenticationRequestDTO;
 import rs.ac.uns.ftn.xws.team22.auth.dto.AuthenticationResponseDTO;
 import rs.ac.uns.ftn.xws.team22.auth.service.impl.AuthenticationService;
@@ -23,4 +21,29 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponseDTO> createAuthenticationToken(@RequestBody AuthenticationRequestDTO dto) {
         return new ResponseEntity<>(authService.login(dto), HttpStatus.OK);
     }
+
+    @GetMapping("/has-admin-role")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<?> hasAdminRole(){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping("/has-system-user-role")
+    @PreAuthorize("hasAuthority('ROLE_SYSTEM_USER')")
+    public ResponseEntity<?> hasSystemUserRole(){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping("/has-agent-role")
+    @PreAuthorize("hasAuthority('ROLE_AGENT')")
+    public ResponseEntity<?> hasAgentRole(){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping("/is-authenticated")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> isAuthenticated(){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
 }
