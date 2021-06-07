de7d549f7ad33dfd7b520c0837bd3850d549847c
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2021-06-07 23:48:38 +0200

    Small improvements

--- front/src/app/profile-page/profile-page.component.ts
+++ front/src/app/profile-page/profile-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RegularUser } from '../model/RegularUserModel';
 import { AuthService } from '../service/authorization/auth.service';
 import { UserService } from '../service/user.service';
@@ -12,21 +12,15 @@ import { UserService } from '../service/user.service';
 export class ProfilePageComponent implements OnInit {
   id: string = '';
   user: RegularUser = new RegularUser();
-  constructor(private route: ActivatedRoute, private userService: UserService, public authService: AuthService) {
+  constructor(private route: ActivatedRoute, private userService: UserService, public authService: AuthService, private router: Router) {
     this.id = route.snapshot.params[`id`];
+    this.route.paramMap.subscribe(params => {this.ngOnInit();})
    }
 
   ngOnInit(): void {
-    this.userService.getUserById(this.id).subscribe((response) => {
+    this.userService.getUserById(this.route.snapshot.params[`id`]).subscribe((response) => {
       this.user = response as RegularUser;
       console.log(this.user);
     })
   }
-  ngOnChanges(): void {
-    this.userService.getUserById(this.id).subscribe((response) => {
-      this.user = response as RegularUser;
-      console.log(this.user);
-    })
-  }
-
 }
