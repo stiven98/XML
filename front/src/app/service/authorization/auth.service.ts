756ccc6e41809e8c53a3d95b6ddade099209b1f1
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2021-06-11 19:34:50 +0200

    Implement https on auth service

--- front/src/app/service/authorization/auth.service.ts
+++ front/src/app/service/authorization/auth.service.ts
@@ -27,8 +27,9 @@ export class AuthService {
   private access_token = null;
 
   signIn(user: AccountInfoModel) {
+    alert("Picka");
     return this.apiService
-      .post('http://localhost:8080/auth', user)
+      .post('http://localhost:8080/adsdsadasdsd', user)
       .subscribe((res: any) => {
         localStorage.setItem('access_token', res.token);
         let authority = res.role;
